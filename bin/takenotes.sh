30c405a490a81f5264de7df42e9e9f1f8889e851
Konrad Förstner <konrad@foerstner.org>
2017-06-17 10:51:30 +0200

    Replace date by dash

--- bin/takenotes.sh
+++ bin/takenotes.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
 DATE=$(date +"%F-%H_%M_%S")
 read -ep "New note: " TEXT
-echo ${DATE} $TEXT > notes/$DATE.txt
+echo '- '$TEXT > notes/$DATE.txt
 echo Done.

657aab9d378aaf7ca0da0c5330cb3a1d3492064a
Konrad Förstner <konrad@foerstner.org>
2017-06-17 10:42:11 +0200

    Add file deletion

--- bin/sendnotes.sh
+++ bin/sendnotes.sh
@@ -1,15 +1,11 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-tar cfvj notes.tar.bz2 ~/notes
+tar cfvj ~/notes.tar.bz2 ~/notes
 gpg --encrypt \
     --trust-model always \
-    --output notes.tar.bz2.gpg \
+    --output ~/notes.tar.bz2.gpg \
     --recipient konrad@foerstner.org \
-    notes.tar.bz2
+    ~/notes.tar.bz2
 
-scp notes.tar.bz2.gpg df:~/
-
-# --default-key 4096R/DF9DB3D3
-
-#gpg  -e 
-
+scp ~/notes.tar.bz2.gpg df:~/
+rm -f ~/notes.tar.bz2 ~/notes.tar.bz2.gpg
